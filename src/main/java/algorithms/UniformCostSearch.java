a6d8a5b1f1e6a2b5d4f50873bbc7d340c1f5bcc3
Kendall Zettlmeier <kendall@zettlmeier.com>
2019-01-20 11:09:15 -0600

    Adds uniform-cost algorithm

--- src/main/java/algorithms/UniformCostSearch.java
+++ src/main/java/algorithms/UniformCostSearch.java
@@ -1,13 +1,41 @@
 package algorithms;
 
+import helpers.Constants;
 import models.Node;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeoutException;
 
 public class UniformCostSearch implements IAlgorithm {
     public List<Node> solve(Node startingNode, int[][] goalState, Date startingTime) throws TimeoutException {
-        throw new UnsupportedOperationException();
+        PriorityQueue<Node> queue = new PriorityQueue<>(Constants.MAX_POSSIBLE_STATES, (x, y) -> y.getBookKeeping().getPathCost() - x.getBookKeeping().getPathCost());
+        List<Node> visitedNodes = new ArrayList<>();
+
+        queue.add(startingNode);
+
+        while (!queue.isEmpty()) {
+            if ((new Date()).getTime() - startingTime.getTime() >= Constants.MAX_TIME_TO_SOLVE) {
+                throw new TimeoutException("Algorithm exceeded time");
+            }
+            // Dequeue
+            Node node = queue.poll();
+
+            // Mark node as visited
+            node.getBookKeeping().setExpanded(true);
+            visitedNodes.add(node);
+            // Check if starting state equals goalState
+            if (node.checkIfStatesAreEqual(goalState)) {
+                return node.getRouteToCurrentNode();
+            }
+
+            List<Node> children = node.getSuccessors(node.getBookKeeping().getAction());
+            for (Node child : children) {
+                if (!child.getBookKeeping().isExpanded() && !visitedNodes.contains(child)) {
+                    queue.add(child);
+                }
+            }
+        }
+
+        return null;
     }
 }
--- src/main/java/helpers/Constants.java
+++ src/main/java/helpers/Constants.java
@@ -2,4 +2,5 @@ package helpers;
 
 public final class Constants {
     public static int MAX_TIME_TO_SOLVE = 5 * 60 * 1000;
+    public static int MAX_POSSIBLE_STATES = 362880;
 }
